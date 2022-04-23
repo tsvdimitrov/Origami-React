ce2cc6fbfd085e176b3e7b6024605c867623f0f2
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-04-23 21:34:01 +0300

    Update App.js adding white-origrami-bird.png

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>Lets begin</h1>
       <nav className={style.navigation}>
         <ul>
+          <li className="listItem"><img src="/white-origami-bird.png" alt="white origami" /></li>
           <li className="listItem"><a href="#">Going to 1</a></li>
           <li className="listItem"><a href="#">Going to 2</a></li>
           <li className="listItem"><a href="#">Going to 3</a></li>

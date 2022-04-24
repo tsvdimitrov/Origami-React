94acd8d31bf600a263a6e877088f64792589dfc2
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-04-24 07:20:12 +0300

    Update Header and Menu

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import style from './Header.module.css';
+
+
+const Header = () => {
+    return (
+        <nav className={style.navigation}>
+            <ul>
+                <li className="listItem"><img src="/white-origami-bird.png" alt="white origami" /></li>
+                <li className="listItem"><a href="#">Going to 1</a></li>
+                <li className="listItem"><a href="#">Going to 2</a></li>
+                <li className="listItem"><a href="#">Going to 3</a></li>
+                <li className="listItem"><a href="#">Going to 4</a></li>
+                <li className="listItem"><a href="#">Going to 5</a></li>
+                <li className="listItem"><a href="#">Going to 6</a></li>
+                <li className="listItem"><a href="#">Going to 7</a></li>
+                <li className="listItem"><a href="#">Going to 8</a></li>
+                <li className="listItem"><a href="#">Going to 9</a></li>
+                <li className="listItem"><a href="#">Going to 10</a></li>
+                <li className="listItem"><a href="#">Going to 11</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Header;

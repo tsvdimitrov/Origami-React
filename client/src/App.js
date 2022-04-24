55283aacbdc1bf5909fcbe16ccad9c2b8f955afb
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-04-24 09:18:36 +0300

    Create and update MenuItem

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import Header from './components/Header/Header.js';
+import Menu from './components/Menu/Menu.js';
 import style from './App.module.css';
 
 function App() {
   return (
     <div className={style.app}>
       <Header />
+      <div className={style.container}>
+        <Menu />
+        <main className="main">
+
+        </main>
+      </div>
     </div >
   );
 }

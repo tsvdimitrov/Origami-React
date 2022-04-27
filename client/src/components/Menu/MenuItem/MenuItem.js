894d904a58646d1073fe44824d722fa07009eee3
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-04-27 22:59:13 +0300

    Update MenuItem.js

--- client/src/components/Menu/MenuItem/MenuItem.js
+++ client/src/components/Menu/MenuItem/MenuItem.js
@@ -1,17 +1,2 @@
 import './MenuItem.css';
 
-const MenuItem = ({
-    id,
-    onClick,
-    children,
-}) => {
-    return (
-        <li className="menu-item">
-            <a href="#" onClick={() => onClick(id)}>
-                {children}
-            </a>
-        </li>
-    )
-};
-
-export default MenuItem;

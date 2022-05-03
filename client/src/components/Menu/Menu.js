3284ff0486248b79668011792a8d159227aa26cc
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-05-03 21:34:18 +0300

    Create MenuConstants and update Menu.js

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -3,20 +3,8 @@ import { useState } from 'react';
 import MenuItem from './MenuItem';
 
 import './Menu.css';
+import { MENU_ITEMS } from './MenuConstants';
 
-const menuItems = [
-    { id: 1, text: 'Going to 1' },
-    { id: 2, text: 'Going to 2' },
-    { id: 3, text: 'Going to 3' },
-    { id: 4, text: 'Going to 4' },
-    { id: 5, text: 'Going to 5' },
-    { id: 6, text: 'Going to 6' },
-    { id: 7, text: 'Going to 7' },
-    { id: 8, text: 'Going to 8' },
-    { id: 9, text: 'Going to 9' },
-    { id: 10, text: 'Going to 10' },
-    { id: 11, text: 'Going to 11' }
-];
 
 const Menu = () => {
 
@@ -24,10 +12,9 @@ const Menu = () => {
 
     return (
         <aside className="menu">
-            {menuItems.map(x =>
+            {MENU_ITEMS.map(x =>
                 < MenuItem key={x.id} id={x.id} onClick={setCurrentItem} >{x.text}</MenuItem>
-            )
-            }
+            )}
         </aside >
     )
 };

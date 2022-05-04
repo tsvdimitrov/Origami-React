c3c4f69e4acfa9174fe31ce58b4afe93611e950a
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-05-04 07:39:31 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -29,15 +29,25 @@ class App extends Component {
     this.setState({ selectedPost: id });
   }
 
+  getPosts() {
+    if (!this.state.selectedPost) {
+      return this.state.posts;
+    } else {
+      return [this.state.post.find(x => x.id == this.state.selectedPost)];
+    }
+  }
+
   render() {
     return (
       <div className={style.app}>
         <Header />
         <div className={style.container}>
-          <Menu />
-          <Main
-            posts={this.state.posts}
+          <Menu
             onMenuItemClick={this.onMenuItemClick.bind(this)}
+          />
+          <Main
+            posts={this.getPosts()}
+
           />
         </div>
       </div >
